6732a427b52ae67a2de658b30315877d6b7e581d
Igor <igor.v.r.silva@alunos.unicerrado.ebu.br>
2026-06-05 18:48:52 -0300

    Fundo

--- src/App.jsx
+++ src/App.jsx
@@ -3,3 +3,11 @@ import AppRoutes from './routes/AppRoutes';
 export default function App() {
   return <AppRoutes />;
 }
+body {
+  margin: 0;
+  background-image: url('./assets/Fundo.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+}
