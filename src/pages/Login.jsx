bb3d719c4fe5e1e7581ae38f6445a2dcc7f9e149
Igor <igor.v.r.silva@alunos.unicerrado.ebu.br>
2026-06-06 14:46:36 -0300

    alterações login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,14 +4,37 @@ function Login() {
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    // aqui você pode validar usuário e senha depois
     navigate("/dashboard");
   };
 
   return (
-    <button onClick={handleLogin}>
-      Entrar
-    </button>
+    <div className="page-container">
+      <nav className="main-nav">
+        <button className="active">Login</button>
+
+        <button onClick={() => navigate("/dashboard")} className="button-link">
+          Dashboard
+        </button>
+
+        <button onClick={() => navigate("/pacientes")} className="button-link">
+          Pacientes
+        </button>
+      </nav>
+
+      <main className="page-card">
+        <span className="page-tag">SIGTEA</span>
+
+        <h1>Login</h1>
+
+        <p>
+          Acesse o sistema para acompanhar pacientes e registrar atendimentos.
+        </p>
+
+        <button onClick={handleLogin}>
+          Entrar
+        </button>
+      </main>
+    </div>
   );
 }
 
