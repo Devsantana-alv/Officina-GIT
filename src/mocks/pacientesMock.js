0934d4a7ba6fb0144ce451e2a988f435bcc74209
Igor <igor.v.r.silva@alunos.unicerrado.ebu.br>
2026-06-06 14:08:27 -0300

    Pacientes

---
+++ src/mocks/pacientesMock.js
@@ -0,0 +1,14 @@
+export const pacientesMock = [
+  {
+    id: 1,
+    nome: "João Silva",
+    cpf: "123.456.789-00",
+    dataNascimento: "1990-05-10"
+  },
+  {
+    id: 2,
+    nome: "Maria Oliveira",
+    cpf: "987.654.321-00",
+    dataNascimento: "1985-09-15"
+  }
+];
